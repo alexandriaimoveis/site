bf659e9e068eb8515afe560cb75b9c278d7ac515
Hebert <hebertdev82@gmail.com>
2026-01-26 15:48:55 -0300

    feat: creating head section

---
+++ app/components/head/page.jsx
@@ -0,0 +1,33 @@
+import { BsPinMapFill } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import { BsEnvelope } from "react-icons/bs";
+
+export default function Head() {
+  return (
+    <section className="px-6 py-2 align-center font-bold shadow-md" style={{ backgroundColor: '#FF0000', color: '#FFFFFF' }}>
+
+      <ul className="flex space-x-8 justify-center">
+        <li>
+          <span className="flex items-center gap-2">
+            <BsPinMapFill />
+            <span>São Lourenço/MG</span>
+          </span>
+        </li>
+
+        <li>
+          <span className="flex items-center gap-2">
+            <BsWhatsapp />
+            (35) 98426-5018
+          </span>
+        </li>
+
+        <li>
+          <span className="flex items-center gap-2">
+            <BsEnvelope />
+            contato@alexandriaimoveis.com.br
+          </span>
+        </li>
+      </ul>
+    </section>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
-export default function Home() {
+import Head from "./components/head/page";
+import Navbar from "./components/navbar/page";
+import Shorts from "./components/shorts/page";
+import Slider from "./components/slider/page";
+
+export default function Page() {
   return (
     <>
-      <h1 className="text-4xl align-center justify-center font-bold">SITE PARA OS CLIENTES</h1>
+      <Head />
+      <Navbar />
+      <Slider />
+      <Shorts />
     </>
   );
 }
