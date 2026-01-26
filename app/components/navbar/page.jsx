de5766be3d51c708c84692abe12b11e31109efe7
Hebert <hebertdev82@gmail.com>
2026-01-26 15:57:40 -0300

    feat: creating navbar section

--- app/components/navbar/page.jsx
+++ app/components/navbar/page.jsx
@@ -1,7 +1,37 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <>
-      Navbar Component
-    </>
+    <section className="flex justify-center py-4 px-6 font-bold shadow-md">
+      <nav>
+        <ul className="flex space-x-16">
+          <li>
+            <Link className="flex items-center gap-2 hover:underline" href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="flex items-center gap-2 hover:underline" href="/">
+              Sobre Nós
+            </Link>
+          </li>
+          <li>
+            <Link className="flex items-center gap-2 hover:underline" href="/">
+              Vendas
+            </Link>
+          </li>
+          <li>
+            <Link className="flex items-center gap-2 hover:underline" href="/">
+              Locação
+            </Link>
+          </li>
+          <li>
+            <Link className="flex items-center gap-2 hover:underline" href="/">
+              Contato
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </section>
   );
 }
