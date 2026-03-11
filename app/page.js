bc0f3b8cb5277a68adb171bda1d3eaeb4884ca64
Hebert <hebertdev82@gmail.com>
2026-03-11 01:06:25 -0300

    feat: counter page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Counter from "./components/counter";
 import Head from "./components/head/page";
 import Navbar from "./components/navbar/page";
 import Shorts from "./components/shorts/page";
@@ -6,10 +7,26 @@ import Slider from "./components/slider/page";
 export default function Page() {
   return (
     <>
-      <Head />
+      {/* <Head />
       <Navbar />
       <Slider />
-      <Shorts />
+      <Shorts /> */}
+
+      <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-black to-gray-900 text-white">
+        <div className="text-center space-y-6">
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Nosso site está quase no ar!
+          </h1>
+          <p className="text-lg md:text-xl">Faltam apenas</p>
+
+          <Counter />
+
+          <p className="text-sm md:text-base">
+            Fique ligado! Estamos preparando algo incrível.
+          </p>
+        </div>
+      </div>
+
     </>
   );
 }
