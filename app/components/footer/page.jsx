c8b378368b57242a34d5cc172aef29c004019449
Hebert <hebertdev82@gmail.com>
2026-04-29 12:59:01 -0300

    feat: contact footer link

--- app/components/footer/page.jsx
+++ app/components/footer/page.jsx
@@ -27,7 +27,7 @@ export default function Footer() {
             <li className="flex items-center gap-2 hover:font-bold transition ease-in-out duration-300 hover:text-[#F29829]"><BiRightArrowAlt /> <Link href="/pages/vendas">Vendas</Link></li>
             <li className="flex items-center gap-2 hover:font-bold transition ease-in-out duration-300 hover:text-[#F29829]"><BiRightArrowAlt /> <Link href="/pages/locacao">Locação</Link></li>
             <li className="flex items-center gap-2 hover:font-bold transition ease-in-out duration-300 hover:text-[#F29829]"><BiRightArrowAlt /> <Link href="/pages/consorcio">Consórcio</Link></li>
-            <li className="flex items-center gap-2 hover:font-bold transition ease-in-out duration-300 hover:text-[#F29829]"><BiRightArrowAlt /> <Link href="/">Contato</Link></li>
+            <li className="flex items-center gap-2 hover:font-bold transition ease-in-out duration-300 hover:text-[#F29829]"><BiRightArrowAlt /> <Link href="/pages/contato">Contato</Link></li>
           </ul>
         </div>
 
